99673cb8479bd10cc9b3d41fa18554c8b9215589
firecharge01 <61401205+firecharge01@users.noreply.github.com>
2024-03-30 21:42:53 -0400

    Add files via upload

--- DevEnvironmentDemo-main/demo.s
+++ DevEnvironmentDemo-main/demo.s
@@ -1,4 +1,4 @@
-;Cruz Y. Diaz Rivera
+;;Sebastián Torrez Segarra 
 .segment "HEADER"
   ; .byte "NES", $1A      ; iNES header identifier
   .byte $4E, $45, $53, $1A
@@ -33,6 +33,13 @@ reset:
   stx $4010 	; disable DMC IRQs
 
 ;; first wait for vblank to make sure PPU is ready
+PPUCTRL   = $2000
+PPUMASK   = $2001
+PPUSTATUS = $2002
+PPUADDR   = $2006
+PPUDATA   = $2007
+OAMADDR   = $2003
+OAMDMA    = $4014
 vblankwait1:
   bit $2002
   bpl vblankwait1
@@ -51,9 +58,10 @@ clear_memory:
   bne clear_memory
 
 ;; second wait for vblank, PPU is ready after this
-vblankwait2:
-  bit $2002
-  bpl vblankwait2
+
+
+
+
 
 main:
 load_palettes:
@@ -73,11 +81,13 @@ load_palettes:
 enable_rendering:
   lda #%10000000	; Enable NMI
   sta $2000
-  lda #%00010000	; Enable Sprites
+  lda #%00001110	; Enable Sprites and background
   sta $2001
 
 forever:
   jmp forever
+
+
 
 nmi:
   ldx #$00 	; Set SPR-RAM address to 0
@@ -85,103 +95,74 @@ nmi:
 @loop:	lda hello, x 	; Load the hello message into SPR-RAM
   sta $2004
   inx
-  cpx #$28
+  cpx #$02
   bne @loop
+  LDA #$00        ;;tell the ppu there is no background scrolling
+ 
+
+  LoadBackground:
+  LDA $2002             ; read PPU status to reset the high/low latch
+  LDA #$20
+  STA $2006             ; write the high byte of $2000 address
+  LDA #$00
+  STA $2006             ; write the low byte of $2000 address
+  LDX #$00              ; start out at 0
+LoadBackgroundLoop:
+  LDA background, x     ; load data from address (background + the value in x)
+  STA $2007             ; write to PPU
+  INX                   ; X = X + 1
+  CPX #$6a                 ; Compare X to hex $80, decimal 128 - copying 128 bytes
+  BNE LoadBackgroundLoop  ; Branch to LoadBackgroundLoop if compare was Not Equal to zero
+                        ; if compare was equal to 128, keep going down
+
+LoadAttribute:
+  LDA $2002             ; read PPU status to reset the high/low latch
+  LDA #$23
+  STA $2006             ; write the high byte of $23C0 address
+  LDA #$c0
+  STA $2006             ; write the low byte of $23C0 address
+  LDX #$00              ; start out at 0
+LoadAttributeLoop:
+  LDA attribute, x      ; load data from address (attribute + the value in x)
+  STA $2007             ; write to PPU
+  INX                   ; X = X + 1
+  CPX #$10              ; Compare X to hex $08, decimal 8 - copying 8 bytes
+  BNE LoadAttributeLoop
+
   rti
 
+
 hello:
-  .byte $00, $00, $00, $00 	; Why do I need these here?
-  .byte $00, $00, $00, $00
-  .byte $6c, $00, $00, $6c
-  .byte $6c, $01, $00, $76
-  .byte $6c, $02, $00, $80
-  .byte $6c, $03, $00, $8A
-  .byte $76, $04, $01, $6c
-  .byte $76, $05, $01, $76
-  .byte $76, $06, $01, $80
-  .byte $76, $03, $01, $8A
 
 palettes:
   ; Background Palette
-  .byte $0f, $00, $00, $00
-  .byte $0f, $00, $00, $00
-  .byte $0f, $00, $00, $00
-  .byte $0f, $00, $00, $00
+  .byte $0f, $37, $28, $30
+  .byte $0f, $38, $2D, $3D
+  .byte $0f, $09, $1a, $24
+  .byte $0f, $2d, $1a, $0C
 
   ; Sprite Palette
-  .byte $0f, $03, $21, $24
-  .byte $0f, $19, $26, $15
-  .byte $0f, $00, $00, $00
-  .byte $0f, $00, $00, $00
+  .byte $0f, $37, $28, $30
+  .byte $0f, $38, $2D, $3D
+  .byte $0f, $09, $1a, $24
+  .byte $0f, $2d, $1a, $0C
+
+background:
+.byte $00,$01,$02,$03,$04,$05,$06,$07,$08,$09,$0a,$0b,$0c,$0d,$0e,$0f
+.byte $20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$2a,$2b,$2c,$2d,$2E,$2f
+.byte $10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$1a,$1b,$1c,$1d,$1E,$1f
+.byte $30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$3a,$3b,$3c,$3d,$3E,$3f
+.byte $40,$41,$42,$43,$44,$45,$46,$47,$48,$49,$4a,$4b,$4c,$4d,$4E,$4f
+.byte $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+.byte $50,$51,$52,$53,$54,$55,$56,$57,$58,$59,$5a,$00,$00,$00,$00,$00
+
+
+
+attribute:
+  .byte %00100100, %00000101, %00000101, %00000101, %0000101, %00000101, %00001101, %00001111, %00000000, %00000000
 
 ; Character memory
 .segment "CHARS"
-  .byte %01111110	; C (00)
-  .byte %11111111
-  .byte %11000011
-  .byte %11000000
-  .byte %11000000
-  .byte %11000011
-  .byte %11111111
-  .byte %01111110
-  .byte $00, $00, $00, $00, $00, $00, $00, $00
+ .incbin "project.chr"
 
-  .byte %11111110	; R (01)
-  .byte %11111111
-  .byte %11000011
-  .byte %11111111
-  .byte %11111110
-  .byte %11001100
-  .byte %11000110
-  .byte %11000011
-  .byte $FE, $FF, $C3, $FF, $FE, $CC, $C6, $C3
 
-  .byte %00000000	; U (02)
-  .byte %00000000
-  .byte %00000000
-  .byte %00000000
-  .byte %00000000
-  .byte %00000000
-  .byte %00000000
-  .byte %00000000
-  .byte $C3, $C3, $C3, $C3, $C3, $C3, $FF, $FF
-
-  .byte %11111111	; Z (03)
-  .byte %11111111
-  .byte %00000110
-  .byte %00001100
-  .byte %00011000
-  .byte %00110000
-  .byte %11111111
-  .byte %11111111
-  .byte $FF, $FF, $06, $0C, $18, $30, $FF, $FF
-
-  .byte %11111110	; D (04)
-  .byte %11111111
-  .byte %11000011
-  .byte %11000011
-  .byte %11000011
-  .byte %11000011
-  .byte %11111111
-  .byte %11111110
-  .byte $FE, $FF, $C3, $C3, $C3, $C3, $FF, $FE
-
-  .byte %01111110	; I (05)
-  .byte %01111110
-  .byte %00011000
-  .byte %00011000
-  .byte %00011000
-  .byte %00011000
-  .byte %01111110
-  .byte %01111110
-  .byte $00, $00, $00, $00, $00, $00, $00, $00
-
-  .byte %00000000	; A (06)
-  .byte %00000000
-  .byte %00000000
-  .byte %00000000
-  .byte %00000000
-  .byte %00000000
-  .byte %00000000
-  .byte %00000000
-  .byte $3C, $66, $C3, $C3, $FF, $C3, $C3, $C3
